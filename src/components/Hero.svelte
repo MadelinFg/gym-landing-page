f2b5db659f85f8846967eb30a6690905a6b28337
Made Fg <madelinfg@gmail.com>
2024-07-29 16:22:40 -0600

    Add main components

---
+++ src/components/Hero.svelte
@@ -0,0 +1,9 @@
+<script>
+    import SectionWrapper from "./SectionWrapper.svelte";
+
+    import Header from "../components/Header.svelte"
+</script>
+
+<SectionWrapper>
+    <Header />
+</SectionWrapper>
