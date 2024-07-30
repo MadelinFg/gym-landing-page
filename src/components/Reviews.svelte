f90895f2afadda05b7e9405becbccb2537495e63
Made Fg <madelinfg@gmail.com>
2024-07-30 12:16:20 -0600

    Update Reviews ans ReviewsCard structures

--- src/components/Reviews.svelte
+++ src/components/Reviews.svelte
@@ -1,5 +1,7 @@
 <script>
     import SectionWrapper from "../components/SectionWrapper.svelte";
+    import ReviewsCard from "./ReviewsCard.svelte";
+    import Stars from "./Stars.svelte";
 
     let lim = true;
 
@@ -79,8 +81,27 @@
 
         <div class="flex flex-col md:flex-row gap-8 w-fit mx-auto">
             <div class="flex flex-col gap-8">
-                
+                {#each lim ? reviewsList.slice(0, 4) : reviewsList as reviewItem, index}
+                <ReviewsCard {reviewItem} left={true} {index} />
+                {/each}
             </div>
+            
+            <div class="w-[1px] bg-slate-950 hidden md:flex"></div>
+            
+            <div class="flex flex-col gap-8">
+                {#each lim ? reviewsList.slice(0, 4) : reviewsList as reviewItem, index}
+                <ReviewsCard {reviewItem} {index} />
+                {/each}
+            </div>
+        </div>
+
+        <button on:click={() => lim = !lim} class="specialBtn">
+            <p>{lim ? "Show more!" : "Show less"}</p>
+        </button>
+
+        <div class="flex flex-col gap-10">
+            <p class="mx-auto text-lg sm:text-xl md:text-2xl font-semibold">Trusted by 1,349 Swoldiers</p>
+            <Stars />
         </div>
     </div>
 </SectionWrapper>
