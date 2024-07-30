34a9c695f01c7c01ca86fab46c60d2230e323254
Made Fg <madelinfg@gmail.com>
2024-07-30 13:13:51 -0600

    Add Footer structure

---
+++ src/components/Footer.svelte
@@ -0,0 +1,30 @@
+<footer class="py-16 sm:py-20 md:py-24 px-4 md:px-8">
+    <div class="max-w-[1200px] mx-auto w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-8 text-base">
+        <div class="flex flex-col gap-4 md:col-span-2">
+            <h1 class="font-semibold">Swoley <span class="text-indigo-400">Moley</span></h1>
+
+            <p>©2023 SWOLEY MOLEY LTD. All rights reserved.</p>
+        </div>
+
+        <div class="flex flex-col gap-4">
+            <p class="font-bold poppins text-base sm:text-lg">Support</p>
+            <a href="mailto:madelinfg@gmail.com" class="cursor-pointer hover:text-indigo-400 duration-200" target="_blank">Contact Us</a>
+        </div>
+        <div class="flex flex-col gap-4">
+            <p class="font-bold poppins text-base sm:text-lg">Research</p>
+            <a href="https://svelte.dev/" class="cursor-pointer hover:text-indigo-400 duration-200" target="_blank">Read the Science</a>
+            <a href="https://www.youtube.com/watch?v=vb7CgDcA_6U" class="cursor-pointer hover:text-indigo-400 duration-200" target="_blank">Watch the Tutorial</a>
+        </div>
+        <div class="flex flex-col gap-4">
+            <p class="font-bold poppins text-base sm:text-lg">Follow us</p>
+            <a href="https://github.com/MadelinFg" class="cursor-pointer hover:text-indigo-400 duration-200" target="_blank">
+                <i class="fa-brands fa-github" />
+                Github
+            </a>
+            <a href="https://www.linkedin.com/in/madelin-flores-1890191a9" class="cursor-pointer hover:text-indigo-400 duration-200" target="_blank">
+                <i class="fa-brands fa-linkedin" />
+                LinkedIn
+            </a>
+        </div>
+    </div>
+</footer>
